9068227390f371985fcd7ba4e39102507f888b21
McTrip777 <mcjakedude@gmail.com>
2019-01-23 10:49:39 -0800

    JS-3 first commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,42 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - is when you are bound to the browser information and objects.
+* 2. Implicit Binding - is when you call a variable within your object with a (this) refering to the object
+* 3. Explicit Binding - is when you call a variable outside of your object using (this).
+* 4. New Binding - is when you identify your attributes with a (this) in a reusable object. 
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
 
+
+
+
+
+// Principle 2
 // code example for Implicit Binding
 
-// Principle 3
 
+
+
+
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
 
+
+
+
+
+// Principle 4
 // code example for Explicit Binding
+
+
+
+
+
